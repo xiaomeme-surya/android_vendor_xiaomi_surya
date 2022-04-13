a7bbcff4aa0e1ce0237c8c134e5e45301d83722c
TheStrechh <thestrechh@gmail.com>
2022-04-13 07:00:25 +0530

    surya: Switch to prebuilt camera provider

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -280,6 +280,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/surya/proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-legacy \
     eglSubDriverAndroid \
     libEGL_adreno \
     libGLESv1_CM_adreno \
